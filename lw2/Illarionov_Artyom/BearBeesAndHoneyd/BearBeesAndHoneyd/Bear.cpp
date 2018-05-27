bb5e08c7194ffcf9fce45ee5d7db0de29d5ab2bd
redrik234 <illarionow.artem@yandex.ru>
2018-05-27 16:56:45 +0300

    реализован класс Bear

--- lw2/Illarionov_Artyom/BearBeesAndHoneyd/BearBeesAndHoneyd/Bear.cpp
+++ lw2/Illarionov_Artyom/BearBeesAndHoneyd/BearBeesAndHoneyd/Bear.cpp
@@ -1,1 +1,17 @@
+#include "stdafx.h"
 #include "Bear.h"
+
+Bear::Bear(HoneyPot & honeyPot)
+	:m_honeyPot(honeyPot)
+{
+}
+
+void Bear::EatHoney()
+{
+	std::cout << "-> Bear eats honey" << std::endl;
+	while (!m_honeyPot.isEmpty())
+	{
+		m_honeyPot.popPortion();
+	}
+
+}
--- lw2/Illarionov_Artyom/BearBeesAndHoneyd/BearBeesAndHoneyd/Bear.h
+++ lw2/Illarionov_Artyom/BearBeesAndHoneyd/BearBeesAndHoneyd/Bear.h
@@ -1,6 +1,11 @@
 #pragma once
+#include "HoneyPot.h"
 
 class Bear
 {
-
+public:
+	Bear(HoneyPot & honeyPot);
+	void EatHoney();
+private:
+	HoneyPot m_honeyPot;
 };
