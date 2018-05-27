fc1bc5921710f54f197a650646c0a044b5621af3
redrik234 <illarionow.artem@yandex.ru>
2018-05-27 16:38:34 +0300

    реализован класс HoneyPot

--- lw2/Illarionov_Artyom/BearBeesAndHoneyd/BearBeesAndHoneyd/HoneyPot.cpp
+++ lw2/Illarionov_Artyom/BearBeesAndHoneyd/BearBeesAndHoneyd/HoneyPot.cpp
@@ -1,1 +1,28 @@
+#include "stdafx.h"
 #include "HoneyPot.h"
+
+HoneyPot::HoneyPot(size_t potSize)
+	:m_potSize(potSize)
+	, m_portions(0)
+{
+}
+
+bool HoneyPot::isEmpty() const
+{
+	return m_potSize == 0;
+}
+
+bool HoneyPot::isFull() const
+{
+	return m_portions == m_potSize;
+}
+
+void HoneyPot::addPortion()
+{
+	++m_portions;
+}
+
+void HoneyPot::popPortion()
+{
+	--m_portions;
+}
--- lw2/Illarionov_Artyom/BearBeesAndHoneyd/BearBeesAndHoneyd/HoneyPot.h
+++ lw2/Illarionov_Artyom/BearBeesAndHoneyd/BearBeesAndHoneyd/HoneyPot.h
@@ -2,5 +2,17 @@
 
 class HoneyPot
 {
+public:
 
+	HoneyPot(size_t potSize);
+
+	bool isEmpty() const;
+	bool isFull() const;
+
+	void addPortion();
+	void popPortion();
+
+private:
+	size_t m_potSize;
+	size_t m_portions;
 };
