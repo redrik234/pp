93a43d3f16345b5da8a379809885cb9e151a1011
redrik234 <illarionow.artem@yandex.ru>
2018-05-29 02:47:28 +0300

    Добавлена имитация полета пчел, диапазон времени от 150 до 800 мс

--- lw2/Illarionov_Artyom/BearBeesAndHoneyd/BearBeesAndHoneyd/Bee.cpp
+++ lw2/Illarionov_Artyom/BearBeesAndHoneyd/BearBeesAndHoneyd/Bee.cpp
@@ -1,5 +1,12 @@
 #include "stdafx.h"
 #include "Bee.h"
+#include <random>
+
+namespace 
+{
+	const size_t MIN_FLIGHT_TIME = 150;
+	const size_t MAX_FLIGHT_TIME = 800;
+}
 
 Bee::Bee(size_t id, HoneyPot & honeyPot, Event & signalForBeesToWork
 		, Event & signalToWakeUpBear, Semaphore & workingHours)
@@ -13,14 +20,24 @@ Bee::Bee(size_t id, HoneyPot & honeyPot, Event & signalForBeesToWork
 
 void Bee::collectsAndBearsHoney()
 {
+	std::mt19937 gen{ std::random_device()() };
+	std::uniform_int_distribution<int> uid(MIN_FLIGHT_TIME, MAX_FLIGHT_TIME);
+
 	while (1)
 	{
+		
+
+		unsigned flightTime = uid(gen);
+
+		Sleep(flightTime);
+
 		m_signalForBeesToWork.wait();
 		m_workingHours.wait();
+
 		if (!m_honeyPot.isFull())
 		{
 			m_honeyPot.addPortion();
-			std::printf("-> Bee #%zu collects and bears one portion honey\n", m_id);
+			std::printf("-> Bee #%zu collects and bears one portion honey. Time spent on collecting honey: %u ms\n", m_id, flightTime);
 		}
 
 		if (m_honeyPot.isFull())
